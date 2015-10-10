0a787e750a87742ce59895cf4f9c1ee389d60bd8
Veta <vetaredkina@gmail.com>
2015-10-10 12:55:02 -0700

    Run_Parsing

--- run.bat
+++ run.bat
@@ -1,9 +1,7 @@
 echo on
 set GITHUB_ACCOUNT=Veta99
 set WS_DIR=Workspace
-set REPO_NAME=E2E_TV
-set APP_VERSION=1.1
-set MAIN_CLASS=core.TV
+set REPO_NAME=Parsing
 if "%JAVA_HOME%" == "" GOTO EXIT_JAVA
 ECHO Java installed
 if "%M2%" == "" GOTO EXIT_MVN
@@ -13,12 +11,18 @@ if NOT %ERRORLEVEL% == 0 GOTO EXIT_GIT
 ECHO Git installed
 GOTO NEXT
 :NEXT
+if NOT EXIST C:\%WS_DIR% GOTO NO_Workspace
+if EXIST C:\%WS_DIR%\%REPO_NAME% RMDIR /S /Q C:\%WS_DIR%\%REPO_NAME%
 CD C:\%WS_DIR%
 git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
 CD %REPO_NAME%
-SLEEP 2
-CALL mvn clean site test -Dtest=AllTests -Dbuild.version="%APP_VERSION%"
 ECHO.
+ECHO Executing Java programm ...
+java -cp C:\%WS_DIR%\%REPO_NAME%\Parsing_DOM_XPath.jar core.DOM_XPath
+java -cp C:\%WS_DIR%\%REPO_NAME%\Parsing_DOM.jar core.DOM
+java -cp C:\%WS_DIR%\%REPO_NAME%\Parsing_SAX.jar core.SAX
+java -cp C:\%WS_DIR%\%REPO_NAME%\Parsing_StAX.jar core.StAX
+GOTO END
 :EXIT_JAVA
 ECHO No Java installed
 GOTO END
@@ -32,4 +36,7 @@ GOTO END
 ECHO %WS_DIR% is not exists
 GOTO END
 :END
-CD C:\
+cd..
+cd..
+
+
